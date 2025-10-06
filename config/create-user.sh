1910095af48ca8932f7bd7858e52659959c1064a
Super User <root@GalsPC.>
2025-10-06 21:41:54 +0300

    Changed base image from bitnami to kafka

--- config/create-user.sh
+++ config/create-user.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 kafka-configs.sh \
-  --bootstrap-server localhost:9094 \
+  --bootstrap-server localhost:9093 \
   --alter --add-config 'SCRAM-SHA-256=[iterations=4096,password=admin-secret]' \
   --user admin
